a95ef22a30e0a84e90f2bb4cc8bdb3334c03b767
rungizmo <41449535+rungizmo@users.noreply.github.com>
2022-08-09 00:33:29 -0700

    Update and rename TTFPP Install.py to DTInstall.py

---
+++ DTInstall.py
@@ -0,0 +1,5 @@
+import os
+os.system("sudo apt-get update")
+os.system("sudo apt-get install pip")
+os.system("pip install TikTokLive")
+os.system("pip install TikTokLive --upgrade")
--- TTFPP Install.py
+++
@@ -1,5 +0,0 @@
-import os
-os.system("sudo apt-get update")
-os.system("sudo apt-get install pip3")
-os.system("pip install TikTokLive")
-os.system("git clone https://github.com/rungizmo/TTFPP.git")
