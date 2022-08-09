b5fff6120a3f6208272e4e5f7bb12a47e38439ac
rungizmo <41449535+rungizmo@users.noreply.github.com>
2022-08-09 00:22:13 -0700

    Add files via upload

---
+++ TTFPP Install.py
@@ -0,0 +1,5 @@
+import os
+os.system("sudo apt-get update")
+os.system("sudo apt-get install pip3")
+os.system("pip install TikTokLive")
+os.system("git clone https://github.com/rungizmo/TTFPP.git")
