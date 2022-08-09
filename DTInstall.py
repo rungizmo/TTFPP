df64f2ab39116e2e9c51869563e8f7d91bc5f456
rungizmo <41449535+rungizmo@users.noreply.github.com>
2022-08-09 00:37:34 -0700

    Update DTInstall.py

--- DTInstall.py
+++ DTInstall.py
@@ -1,5 +1,5 @@
 import os
 os.system("sudo apt-get update")
 os.system("sudo apt-get -y install python3-pip")
-os.system("pip install TikTokLive")
-os.system("pip install TikTokLive --upgrade")
+os.system("pip3 install TikTokLive")
+os.system("pip3 install TikTokLive --upgrade")
